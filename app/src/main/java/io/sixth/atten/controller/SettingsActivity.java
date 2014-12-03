c59d65cf56f46c706caef8521468c64ab954d78f
Shantanu <shantanuraj@ovi.com>
2014-12-04 03:12:42 +0530

    Add twitter links on button click.

--- app/src/main/java/io/sixth/atten/controller/SettingsActivity.java
+++ app/src/main/java/io/sixth/atten/controller/SettingsActivity.java
@@ -1,7 +1,11 @@
 package io.sixth.atten.controller;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import butterknife.OnClick;
 import io.sixth.atten.R;
 import io.sixth.atten.util.BaseActivity;
 
@@ -9,6 +13,20 @@ import io.sixth.atten.util.BaseActivity;
  * Created by walle on 04/12/14.
  */
 public class SettingsActivity extends BaseActivity {
+
+    @OnClick(R.id.atten_btn_shantanu) void clickedShantanu() {
+        Uri url = Uri.parse("http://twitter.com/shantanuraj");
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(url);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.atten_btn_swapnil) void clickedSwapnil() {
+        Uri url = Uri.parse("http://twitter.com/raj_swapnil");
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(url);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
